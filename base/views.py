b1dd6139429b6a1ecc8d70d71c9277e9d6ca8b78
amoobarsam <141170967+amoobarsam@users.noreply.github.com>
2024-05-14 07:59:07 +0330

    ccdd
    ccsss

--- base/views.py
+++ base/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+
+from django.views.generic.detail import DetailView 
+from django.views.generic.edit import  CreateView
+
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -20,6 +25,9 @@ class TaskCreate(CreateView):
     fields = '__all__'  
     success_url = reverse_lazy('tasks')  
 
-    
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task')
     
 
